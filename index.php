c5275ddd813ba79a793caca60d03d9e9da17cb70
jacob9706 <jacob9706@gmail.com>
2012-11-22 17:11:15 -0800

    Updated README

--- index.php
+++ index.php
@@ -5,13 +5,23 @@
  * URL: index.php
  */
 
+/*********************************************
+ * Define a directory separator
+ *********************************************/
 define('DS', DIRECTORY_SEPARATOR);
 
+
+/*********************************************
+ * Require the nessesary files
+ *********************************************/
 require_once 'system/core/Router.php';
 require_once 'system/core/Model_Template.php';
 require_once 'system/core/View_Template.php';
 require_once 'system/core/Controller_Template.php';
 
-$router = new Router();
 
+/*********************************************
+ * Create a new Router and route
+ *********************************************/
+$router = new Router();
 $router->route();
