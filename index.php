0b586df89d5c65c570d11b28580c927fa60dbde8
jacob9706 <jacob9706@gmail.com>
2012-11-25 17:02:22 -0800

    Updated DS

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ define('DS', DIRECTORY_SEPARATOR);
 /*********************************************
  * Require the nessesary files
  *********************************************/
-require_once 'system/config/setup.php';
-require_once 'system/core/Alerts.php';
-require_once 'system/core/Router.php';
-require_once 'system/core/Model_Template.php';
-require_once 'system/core/View_Template.php';
-require_once 'system/core/Controller_Template.php';
+require_once 'system' . DS . 'config' . DS . 'setup.php';
+require_once 'system' . DS . 'core' . DS . 'Alerts.php';
+require_once 'system' . DS . 'core' . DS . 'Router.php';
+require_once 'system' . DS . 'core' . DS . 'Model_Template.php';
+require_once 'system' . DS . 'core' . DS . 'View_Template.php';
+require_once 'system' . DS . 'core' . DS . 'Controller_Template.php';
 
 
 /*********************************************
